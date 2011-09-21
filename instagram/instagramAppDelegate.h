f88ee14e964683fcc5e09ba4bd5edfa3c3e15dea
hongjunkim <jun0683@gmail.com>
2011-09-21 17:04:01 +0900

    Initial Commit

---
+++ instagram/instagramAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  instagramAppDelegate.h
+//  instagram
+//
+//  Created by kim hongjun on 11. 9. 21..
+//  Copyright 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class instagramViewController;
+
+@interface instagramAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet instagramViewController *viewController;
+
+@end
---
+++ instagram/instagramViewController.h
@@ -0,0 +1,13 @@
+//
+//  instagramViewController.h
+//  instagram
+//
+//  Created by kim hongjun on 11. 9. 21..
+//  Copyright 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface instagramViewController : UIViewController
+
+@end
